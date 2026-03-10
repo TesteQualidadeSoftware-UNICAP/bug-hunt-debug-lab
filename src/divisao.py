93f101f17344ef7170f9fc8559e0d26c9544405c
Ivna Valença <ivnavalenca@users.noreply.github.com>
2026-03-09 21:56:51 -0300

    Update divisao.py

--- src/divisao.py
+++ src/divisao.py
@@ -1,8 +1,12 @@
 def dividir(a, b):
 
+    # Técnica de Debug: verificar valores antes da operação
+    print("[DEBUG] valor de a:", a)
+    print("[DEBUG] valor de b:", b)
+
+    # Possível erro se b for zero
     return a / b
 
 
-if __name__ == "__main__":
-
-    print(dividir(10,0))
+# Teste do comportamento
+print(dividir(10, 0))
