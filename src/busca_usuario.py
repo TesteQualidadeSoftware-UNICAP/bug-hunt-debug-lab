2425c7e55b37524bc97450094eb54dc362f2c046
Ivna Valença <ivnavalenca@users.noreply.github.com>
2026-03-09 21:56:31 -0300

    Update busca_usuario.py

--- src/busca_usuario.py
+++ src/busca_usuario.py
@@ -1,9 +1,17 @@
 def buscar_usuario(usuarios, nome):
 
+    # Técnica de Debug: Caso mínimo de reprodução
+    # Testar a função com um conjunto pequeno de dados
+
+    print("[DEBUG] lista de usuarios:", usuarios)
+    print("[DEBUG] usuario buscado:", nome)
+
     for usuario in usuarios:
+
+        print("[DEBUG] verificando:", usuario)
 
         if usuario == nome:
             return True
 
-    # BUG
+    # BUG: deveria retornar False
     return True
