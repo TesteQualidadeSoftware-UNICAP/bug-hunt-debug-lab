129337cbf775eed1de7829426fd2dea011b32783
Ivna Valença <ivnavalenca@users.noreply.github.com>
2026-03-09 21:39:01 -0300

    Refactor calcular_media function
    Refactor media calculation to use sum function and fix division bug.

--- src/media.py
+++ src/media.py
@@ -1,18 +1,8 @@
 def calcular_media(notas):
 
-    total = 0
-
-    for nota in notas:
-        total += nota
+    total = sum(notas)
 
     # BUG
     media = total / (len(notas) - 1)
 
     return media
-
-
-if __name__ == "__main__":
-
-    notas = [8,7,9,6]
-
-    print("Média:", calcular_media(notas))
